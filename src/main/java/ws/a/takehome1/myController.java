5e03d12c56907e9973c41f7b7c92bb3d028c8648
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2022-10-29 19:51:38 +0700

    Menambahkan controller, index.html, dan viewpage.html

---
+++ src/main/java/ws/a/takehome1/myController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.a.takehome1;
+
+/**
+ *
+ * @author fuadi
+ */
+public class myController {
+    
+}
